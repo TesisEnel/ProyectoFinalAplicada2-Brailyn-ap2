8ad3235c266934d457f959e261f6bdb5efd52d74
Carlita31 <carlalopez3105@gmail.com>
2024-11-02 23:49:25 -0400

    Amuenta Version de compileSdk 34 -> 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 
 android {
     namespace = "ucne.edu.proyectofinalaplicada2"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "ucne.edu.proyectofinalaplicada2"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
