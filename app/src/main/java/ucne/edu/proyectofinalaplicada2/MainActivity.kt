06f4bc873d1d880a94a5625b7e02dc4078fe74ec
Carlita31 <carlalopez3105@gmail.com>
2024-11-02 23:47:51 -0400

    Configura AndoridEntryPoint en el MainActivity

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/MainActivity.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/MainActivity.kt
@@ -11,37 +11,28 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import ucne.edu.proyectofinalaplicada2.navigation.RentCarNavHost
 import ucne.edu.proyectofinalaplicada2.ui.theme.ProyectoFinalAplicada2Theme
-
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ProyectoFinalAplicada2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navController = rememberNavController()
+                RentCarNavHost(navController)
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ProyectoFinalAplicada2Theme {
-        Greeting("Android")
+
     }
 }
