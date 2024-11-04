524b29df2d7e1418bd60eb5716784c0a4738b224
Eliezer Terrero Rojas <eliezer_terrero@ucne.edu.do>
2024-11-04 15:31:29 -0400

    Agrega el ViewModel de Vehiculo

---
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/vehiculo/VehiculoViewModel.kt
@@ -0,0 +1,165 @@
+package ucne.edu.proyectofinalaplicada2.presentation.vehiculo
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import ucne.edu.proyectofinalaplicada2.repository.VehiculoRepository
+import ucne.edu.proyectofinalaplicada2.utils.Resource
+import javax.inject.Inject
+
+@HiltViewModel
+class VehiculoViewModel @Inject constructor(
+    private val vehiculoRepository: VehiculoRepository
+): ViewModel() {
+    private val _uistate = MutableStateFlow(Uistate())
+    val uistate = _uistate.asStateFlow()
+
+
+    init {
+        getVehiculos()
+    }
+
+    private fun getVehiculos(){
+        viewModelScope.launch {
+            vehiculoRepository.getVehiculos().collect { result ->
+
+                when (result) {
+                    is Resource.Error -> {
+                        _uistate.update {
+                            it.copy(
+                                error = result.message ?: "Error"
+                            )
+                        }
+                    }
+
+                    is Resource.Loading -> {
+                        _uistate.update {
+                            it.copy(
+                                isLoading = true
+                            )
+                        }
+                    }
+                    is Resource.Success -> {
+                        _uistate.update {
+                            it.copy(
+                                vehiculos = result.data ?: emptyList()
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun save(){
+        viewModelScope.launch {
+            val vehiculo = vehiculoRepository.addVehiculo(uistate.value.toEntity())
+
+            vehiculo.collect { result ->
+                when (result) {
+                    is Resource.Error -> {
+                        _uistate.update {
+                            it.copy(
+                                error = result.message ?: "Error"
+                            )
+                        }
+                    }
+                    is Resource.Loading -> {
+                        _uistate.update {
+                            it.copy(
+                                isLoading = true
+                            )
+                        }
+                    }
+                    is Resource.Success -> {
+                        _uistate.update {
+                            it.copy(
+                                success = "Vehiculo agregado"
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun onChangeMarca(marca: String){
+        _uistate.update {
+            it.copy(
+                marca = marca
+            )
+        }
+    }
+
+    private fun onChangeModelo(modelo: String){
+        _uistate.update {
+            it.copy(
+                modelo = modelo
+                )
+        }
+    }
+
+    private fun onChangePrecio(precio: Int){
+        _uistate.update {
+            it.copy(
+                precio = precio
+            )
+        }
+    }
+
+    private fun onChangeDescripcion(descripcion: String) {
+        _uistate.update {
+            it.copy(
+                descripcion = descripcion
+            )
+        }
+    }
+    private fun onChangeTipoCombustibleId(tipoCombustibleId: Int) {
+        _uistate.update {
+            it.copy(
+                tipoCombustibleId = tipoCombustibleId
+            )
+        }
+
+    }
+    private fun onChangeTipoVehiculoId(tipoVehiculoId: Int) {
+        _uistate.update {
+            it.copy(
+                tipoVehiculoId = tipoVehiculoId
+            )
+        }
+    }
+
+    fun nuevo() {
+        _uistate.update {
+            it.copy(
+                vehiculoId = null,
+                tipoCombustibleId = null,
+                tipoVehiculoId = null,
+                marca = "",
+                modelo = "",
+                precio = 0,
+                descripcion = "",
+                success = "",
+                error = "",
+            )
+        }
+    }
+
+    fun onEvent(event: VehiculoEvent) {
+        when (event) {
+            is VehiculoEvent.OnchangeDescripcion -> onChangeDescripcion(event.descripcion)
+            is VehiculoEvent.OnchangeMarca -> onChangeMarca(event.marca)
+            is VehiculoEvent.OnchangeModelo -> onChangeModelo(event.modelo)
+            is VehiculoEvent.OnchangePrecio -> onChangePrecio(event.precio)
+            is VehiculoEvent.OnchangeTipoCombustibleId -> onChangeTipoCombustibleId(event.tipoCombustibleId)
+            is VehiculoEvent.OnchangeTipoVehiculoId -> onChangeTipoVehiculoId(event.tipoVehiculoId)
+            VehiculoEvent.Save -> save()
+        }
+    }
+
+}
