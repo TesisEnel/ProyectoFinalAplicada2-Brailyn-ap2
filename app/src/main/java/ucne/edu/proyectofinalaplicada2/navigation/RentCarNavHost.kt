d919f84b39a3b32e09d0eabbd99d4ed001982f3c
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-11-05 20:13:11 -0400

    Agrega un poco de estilos al topBar

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/navigation/RentCarNavHost.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/navigation/RentCarNavHost.kt
@@ -3,23 +3,24 @@ package ucne.edu.proyectofinalaplicada2.navigation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -43,8 +44,8 @@ fun RentCarNavHost(
     ) {
         Scaffold(
             topBar = {
-                TopAppBar(
-                    title = { Text("") },
+                CenterAlignedTopAppBar(
+                    title = { Text("BravquezRentcar") },
                     navigationIcon = {
                         IconButton(onClick = {
                             coroutineScope.launch {
@@ -53,7 +54,15 @@ fun RentCarNavHost(
                         }) {
                             Icon(Icons.Rounded.Menu, contentDescription = "Menu Botton")
                         }
-                    }
+                    },
+                    colors = TopAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                        actionIconContentColor = MaterialTheme.colorScheme.onPrimary,
+                        scrolledContainerColor = MaterialTheme.colorScheme.onPrimary,
+                        navigationIconContentColor = MaterialTheme.colorScheme.onPrimary
+                    )
+
                 )
             }
         ) { innerpadding ->
@@ -65,8 +74,8 @@ fun RentCarNavHost(
                 composable<Screen.Home> {
                     Box(
                         modifier = Modifier
-                            .padding(16.dp)
                             .fillMaxWidth()
+                            .verticalScroll(rememberScrollState())
                     ) {
                         Home()
                     }
