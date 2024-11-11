5a354480da4a89b765eebb2763652f7a6de0dfc7
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-11-10 21:56:05 -0400

    Agrega screen de VehiculoRegistro

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/navigation/Screen.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/navigation/Screen.kt
@@ -5,4 +5,6 @@ import kotlinx.serialization.Serializable
 sealed class Screen {
     @Serializable
     data object Home : Screen()
+    @Serializable
+    data object VehiculoRegistroScreen: Screen()
 }
