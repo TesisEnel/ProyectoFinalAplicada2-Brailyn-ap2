d7eae429c016c139135a8841641fe9e53611e74c
Eliezer Terrero Rojas <eliezerterrero12@gmail.com>
2024-11-11 18:41:50 -0400

    Agrega TipoVehiculoRepository.kt

---
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/repository/TipoVehiculoRepository.kt
@@ -0,0 +1,26 @@
+package ucne.edu.proyectofinalaplicada2.repository
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import ucne.edu.proyectofinalaplicada2.data.remote.RentCarRemoteDataSource
+import ucne.edu.proyectofinalaplicada2.data.remote.dto.TipoCombustibleDto
+import ucne.edu.proyectofinalaplicada2.data.remote.dto.TipoVehiculoDto
+import ucne.edu.proyectofinalaplicada2.utils.Resource
+import javax.inject.Inject
+
+class TipoVehiculoRepository @Inject constructor(
+    private val rentCarRemoteDataSource: RentCarRemoteDataSource
+) {
+    fun getTiposVehiculos(): Flow<Resource<List<TipoVehiculoDto>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val tiposVehiculos = rentCarRemoteDataSource.getTiposVehiculos()
+            emit(Resource.Success(tiposVehiculos))
+        } catch (e: HttpException) {
+            emit(Resource.Error("Error de internet ${e.message}"))
+        } catch (e: Exception) {
+            emit(Resource.Error("Error desconocido ${e.message}"))
+        }
+    }
+}
