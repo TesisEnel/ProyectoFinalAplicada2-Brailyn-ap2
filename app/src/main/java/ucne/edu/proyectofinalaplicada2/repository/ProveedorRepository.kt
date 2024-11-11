684f44ae7b4462b015a18dd5d4fba26cd8f34bca
Eliezer Terrero Rojas <eliezerterrero12@gmail.com>
2024-11-11 18:41:10 -0400

    Agrega ProveedorRepository

---
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/repository/ProveedorRepository.kt
@@ -0,0 +1,27 @@
+package ucne.edu.proyectofinalaplicada2.repository
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import ucne.edu.proyectofinalaplicada2.data.remote.RentCarRemoteDataSource
+import ucne.edu.proyectofinalaplicada2.data.remote.dto.ProveedorDto
+import ucne.edu.proyectofinalaplicada2.data.remote.dto.TipoCombustibleDto
+import ucne.edu.proyectofinalaplicada2.utils.Resource
+import javax.inject.Inject
+
+class ProveedorRepository @Inject constructor(
+    private val rentCarRemoteDataSource: RentCarRemoteDataSource
+) {
+    fun getProveedores(): Flow<Resource<List<ProveedorDto>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val proveedores = rentCarRemoteDataSource.getProveedores()
+            emit(Resource.Success(proveedores))
+        } catch (e: HttpException) {
+            emit(Resource.Error("Error de internet ${e.message}"))
+        } catch (e: Exception) {
+            emit(Resource.Error("Error desconocido ${e.message}"))
+        }
+    }
+
+}
