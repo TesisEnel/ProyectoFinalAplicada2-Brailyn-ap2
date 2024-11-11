353b8e2421e0295a12b1909d08c3b1724723ba10
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-11-10 20:33:30 -0400

    Elimina onChange de marca y modelo temporalmente

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/vehiculo/VehiculoEvent.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/vehiculo/VehiculoEvent.kt
@@ -2,8 +2,6 @@ package ucne.edu.proyectofinalaplicada2.presentation.vehiculo
 
 sealed interface VehiculoEvent {
     data object Save : VehiculoEvent
-    data class OnchangeMarca(val marca: String) : VehiculoEvent
-    data class OnchangeModelo(val modelo: String) : VehiculoEvent
     data class OnchangePrecio(val precio: Int) : VehiculoEvent
     data class OnchangeDescripcion(val descripcion: String) : VehiculoEvent
     data class OnchangeTipoCombustibleId(val tipoCombustibleId: Int) : VehiculoEvent
