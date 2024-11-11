ff7ab2762ac71ee88cffa901437e607514edef7a
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-11-10 21:52:22 -0400

    Agrega propiedad de tipo Screen

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/navigation/BottomNavigationItem.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/navigation/BottomNavigationItem.kt
@@ -1,9 +1,11 @@
 package ucne.edu.proyectofinalaplicada2.presentation.navigation
 
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavHostController
 
 data class BottomNavigationItem (
     val title: String,
     val selectedIcon: ImageVector,
     val unSelectedIcon: ImageVector,
+    val screen: Screen
 )
