3edd47de3e28d79a7a5f9aba196abf268916de09
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-11-10 21:54:42 -0400

    Elimina unused imports

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/navigation/BottomNavigationItem.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/navigation/BottomNavigationItem.kt
@@ -1,7 +1,6 @@
 package ucne.edu.proyectofinalaplicada2.presentation.navigation
 
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.navigation.NavHostController
 
 data class BottomNavigationItem (
     val title: String,
