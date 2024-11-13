7a8d61e59e23232906429bcd896c8940a67b1342
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-11-13 15:10:57 -0400

    Modifica para que sea MutableIntStateOf

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/navigation/RentCarNavHost.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/navigation/RentCarNavHost.kt
@@ -15,7 +15,6 @@ import androidx.compose.material.icons.outlined.Call
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -24,11 +23,9 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarColors
-import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -45,7 +42,7 @@ fun RentCarNavHost(
     navHostController: NavHostController,
 ) {
     var selectedItemIndex by rememberSaveable {
-        mutableStateOf(0)
+        mutableIntStateOf(0)
     }
     Scaffold(
         topBar = {
