8393747acb767c83356b77377bb5f4fa53c73d54
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-11-13 15:06:51 -0400

    Agrega variable booleana para manejar cuando se obtiene el permiso

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/permisos/PermisoGallery.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/permisos/PermisoGallery.kt
@@ -14,28 +14,29 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.core.app.ActivityCompat
 import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
 import androidx.lifecycle.viewmodel.compose.viewModel
 import ucne.edu.proyectofinalaplicada2.mainViewModel.MainViewModel
-import ucne.edu.proyectofinalaplicada2.presentation.vehiculo.VehiculoViewModel
+import ucne.edu.proyectofinalaplicada2.presentation.vehiculo.SelectSingleImage
 
 
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @Composable
-fun PermisoGallery(
-){
+fun PermisoGallery() {
 
     val context = LocalContext.current
     val activity = context as? Activity
 
+    var granted = remember { mutableStateOf(false) }
     val viewModel = viewModel<MainViewModel>()
     val dialogQueue = viewModel.visiblePermissionDialogQueue
     val permissionsToRequest = arrayOf(
@@ -51,6 +52,7 @@ fun PermisoGallery(
                 permission = Manifest.permission.READ_MEDIA_IMAGES,
                 isGranted = isGranted
             )
+            granted.value = isGranted
         }
     )
 
@@ -71,13 +73,18 @@ fun PermisoGallery(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = {
-            cameraPermissionResultLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
-        }) {
-            Text(text = "Request Camera Permission")
+
+        if (!granted.value) { // Solo muestra el botón si el permiso no ha sido concedido
+            OutlinedButton(onClick = {
+                cameraPermissionResultLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+            }) {
+                Text(text = "Seleccionar imagem")
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+        } else {
+            SelectSingleImage()
         }
         Spacer(modifier = Modifier.height(16.dp))
-
     }
 
     // Permission Dialogs
@@ -86,7 +93,7 @@ fun PermisoGallery(
         .forEach { permission ->
             PermissionDialog(
                 permissionTextProvider = when (permission) {
-                    Manifest.permission.READ_MEDIA_IMAGES-> GalleryPermissionTextProvider()
+                    Manifest.permission.READ_MEDIA_IMAGES -> GalleryPermissionTextProvider()
                     else -> return@forEach
                 },
                 isPermanentlyDeclined = activity?.let {
