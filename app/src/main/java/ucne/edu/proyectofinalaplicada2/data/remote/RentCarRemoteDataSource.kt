e2991b7258044ca4144736cefb06f5c31cf3c05b
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-11-14 00:12:31 -0400

    Agrega todos los campos por parametro para agregar correctamente el vehiculo

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/data/remote/RentCarRemoteDataSource.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/data/remote/RentCarRemoteDataSource.kt
@@ -17,27 +17,54 @@ class RentCarRemoteDataSource @Inject constructor(
     //cliente
     suspend fun getClientes() = rentCarApi.getClientes()
     suspend fun addCliente(clienteDto: ClienteDto) = rentCarApi.addCliente(clienteDto)
-    suspend fun updateCliente(id: Int ,clienteDto: ClienteDto) = rentCarApi.updateCliente(id, clienteDto)
+    suspend fun updateCliente(id: Int, clienteDto: ClienteDto) =
+        rentCarApi.updateCliente(id, clienteDto)
 
     //renta
     suspend fun getRentas() = rentCarApi.getRentas()
     suspend fun addRenta(rentaDto: RentaDto) = rentCarApi.addRenta(rentaDto)
-    suspend fun updateRenta(id: Int ,rentaDto: RentaDto) = rentCarApi.updateRenta(id, rentaDto)
+    suspend fun updateRenta(id: Int, rentaDto: RentaDto) = rentCarApi.updateRenta(id, rentaDto)
 
     //vehiculo
     suspend fun getVehiculos() = rentCarApi.getVehiculos()
-    suspend fun addVehiculo( precio: RequestBody, descripcion: RequestBody, image: MultipartBody.Part) = rentCarApi.addVehiculo(precio, descripcion, image)
-    suspend fun updateVehiculo(id: Int, vehiculoDto: VehiculoDto) = rentCarApi.updateVehiculo(id, vehiculoDto)
+    suspend fun addVehiculo(
+
+        tipoCombustibleId: RequestBody,
+        tipoVehiculoId: RequestBody,
+        proveedorId: RequestBody,
+        precio: RequestBody,
+        descripcion: RequestBody,
+        marcaId: RequestBody,
+        modeloId: RequestBody,
+        image: MultipartBody.Part,
+
+    ) = rentCarApi.addVehiculo(
+
+        tipoCombustibleId,
+        tipoVehiculoId,
+        proveedorId,
+        precio,
+        descripcion,
+        marcaId,
+        modeloId,
+        image
+    )
+
+    suspend fun updateVehiculo(id: Int, vehiculoDto: VehiculoDto) =
+        rentCarApi.updateVehiculo(id, vehiculoDto)
 
     //Marca
     suspend fun getMarcas() = rentCarApi.getMarcas()
     suspend fun addMarca(marcaDto: MarcaDto) = rentCarApi.addMarca(marcaDto)
-    suspend fun updateMarca(id: Int ,marcaDto: MarcaDto) = rentCarApi.updateMarca(id, marcaDto)
+    suspend fun updateMarca(id: Int, marcaDto: MarcaDto) = rentCarApi.updateMarca(id, marcaDto)
 
     //TipoCombustible
     suspend fun getTiposCombustibles() = rentCarApi.getTipoCombustibles()
-    suspend fun addTipoCombustible(tipoCombustibleDto: TipoCombustibleDto) = rentCarApi.addTipoCombustible(tipoCombustibleDto)
-    suspend fun updateTipoCombustible(id: Int ,tipoCombustibleDto: TipoCombustibleDto) = rentCarApi.updateTipoCombustible(id, tipoCombustibleDto)
+    suspend fun addTipoCombustible(tipoCombustibleDto: TipoCombustibleDto) =
+        rentCarApi.addTipoCombustible(tipoCombustibleDto)
+
+    suspend fun updateTipoCombustible(id: Int, tipoCombustibleDto: TipoCombustibleDto) =
+        rentCarApi.updateTipoCombustible(id, tipoCombustibleDto)
 
     //TipoVehiculo
     suspend fun getTiposVehiculos() = rentCarApi.getTipoVehiculos()
