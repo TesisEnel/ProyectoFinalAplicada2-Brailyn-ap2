09a11ad8cfd1c2e47d59b49339a952a5ac34ad34
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-11-17 20:10:11 -0400

    Agrega GetModelos al onEvent

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/vehiculo/VehiculoEvent.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/vehiculo/VehiculoEvent.kt
@@ -5,6 +5,7 @@ import java.io.File
 sealed interface VehiculoEvent {
     data object Save : VehiculoEvent
     data class OnChangePrecio(val precio: Int) : VehiculoEvent
+    data class GetModelos(val marcaId: Int) : VehiculoEvent
     data class OnChangeDescripcion(val descripcion: String) : VehiculoEvent
     data class OnChangeTipoCombustibleId(val tipoCombustibleId: Int) : VehiculoEvent
     data class OnChangeTipoVehiculoId(val tipoVehiculoId: Int) : VehiculoEvent
