477763b4f6e10052a999c94d0e8500c19782238c
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-11-20 23:13:55 -0400

    Agrega dependecias para usar horizontalpager

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,8 +97,12 @@ dependencies {
 
 
 
+    //Accompanish
+    implementation (libs.accompanist.pager)
+    implementation (libs.accompanist.pager.indicators)
 
-        implementation(libs.androidx.material.icons.extended)
+
+    implementation(libs.androidx.material.icons.extended)
 
     //Credential Manager
     implementation(libs.androidx.credentials)
@@ -123,7 +127,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
 
 
 }
