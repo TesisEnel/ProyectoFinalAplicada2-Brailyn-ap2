e0c8ef79638ecc5e2f50c400a9724c281d1c95b2
Eliezer Terrero Rojas <eliezerterrero12@gmail.com>
2024-11-23 17:08:30 -0400

    Elimina GetModelos y OnChangeMarcaId de VehiculoEvent

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/vehiculo/VehiculoEvent.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/vehiculo/VehiculoEvent.kt
@@ -6,11 +6,9 @@ sealed interface VehiculoEvent {
     data object Save : VehiculoEvent
     data object GetVehiculos : VehiculoEvent
     data class OnChangePrecio(val precio: Int) : VehiculoEvent
-    data class GetModelos(val marcaId: Int) : VehiculoEvent
     data class OnChangeDescripcion(val descripcion: String) : VehiculoEvent
     data class OnChangeTipoCombustibleId(val tipoCombustibleId: Int) : VehiculoEvent
     data class OnChangeTipoVehiculoId(val tipoVehiculoId: Int) : VehiculoEvent
-    data class OnChangeMarcaId(val marcaId: Int): VehiculoEvent
     data class OnChangeProveedorId(val proveedorId: Int): VehiculoEvent
     data class OnChangeModeloId(val modeloId: Int): VehiculoEvent
     data class OnChangeAnio(val anio: Int): VehiculoEvent
