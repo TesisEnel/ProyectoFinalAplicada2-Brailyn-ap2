7afa9fddc0c060939d4c48c44b67a7314b366943
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-11-23 19:07:32 -0400

    Agrega el metodo getclienteByEmail

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/repository/ClienteRepository.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/repository/ClienteRepository.kt
@@ -1,11 +1,11 @@
 package ucne.edu.proyectofinalaplicada2.repository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
 import ucne.edu.proyectofinalaplicada2.data.remote.RentCarRemoteDataSource
 import ucne.edu.proyectofinalaplicada2.data.remote.dto.ClienteDto
 import ucne.edu.proyectofinalaplicada2.utils.Resource
 import javax.inject.Inject
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import retrofit2.HttpException
 
 class ClienteRepository @Inject constructor(
     private  val rentCarRemoteDataSource: RentCarRemoteDataSource
@@ -41,6 +41,18 @@ class ClienteRepository @Inject constructor(
             val cliente = rentCarRemoteDataSource.updateCliente(id,clienteDto)
             emit(Resource.Success(cliente))
             } catch (e: HttpException) {
+            emit(Resource.Error("Error de internet ${e.message}"))
+        } catch (e: Exception) {
+            emit(Resource.Error("Error desconocido ${e.message}"))
+        }
+    }
+
+    fun getClienteByEmail(email: String): Flow<Resource<ClienteDto>> = flow {
+        try {
+            emit(Resource.Loading())
+            val cliente = rentCarRemoteDataSource.getClienteByEmail(email)
+            emit(Resource.Success(cliente))
+        } catch (e: HttpException) {
             emit(Resource.Error("Error de internet ${e.message}"))
         } catch (e: Exception) {
             emit(Resource.Error("Error desconocido ${e.message}"))
