b03541a3778d6dfc2192e4e87c2b6b889f2b1639
Eliezer Terrero Rojas <eliezerterrero12@gmail.com>
2024-11-23 17:09:23 -0400

    Elimina las listas de tipoCombustibles, TiposVehiculos y modelos

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/vehiculo/VehiculoUistate.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/vehiculo/VehiculoUistate.kt
@@ -7,7 +7,7 @@ import ucne.edu.proyectofinalaplicada2.data.remote.dto.TipoCombustibleDto
 import ucne.edu.proyectofinalaplicada2.data.remote.dto.TipoVehiculoDto
 import ucne.edu.proyectofinalaplicada2.data.remote.dto.VehiculoDto
 
-data class Uistate(
+data class VehiculoUistate(
     val vehiculoId: Int? = null,
     val tipoCombustibleId: Int? = null,
     val tipoVehiculoId: Int? = null,
@@ -23,11 +23,6 @@ data class Uistate(
     val error: String = "",
     val success: String = "",
     val marcas: List<MarcaDto> = emptyList(),
-    val tiposCombustibles: List<TipoCombustibleDto> = emptyList(),
     val vehiculos: List<VehiculoDto> = emptyList(),
-    val tiposVehiculos: List<TipoVehiculoDto> = emptyList(),
-    val proveedores: List<ProveedorDto> = emptyList(),
-    val modelos: List<ModeloDto> = emptyList(),
-
     )
 
