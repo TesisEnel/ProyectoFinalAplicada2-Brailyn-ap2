2b076ae7bcd6ef147f513b4bac9f9799ca598445
Eliezer Terrero Rojas <eliezerterrero12@gmail.com>
2024-11-22 22:00:52 -0400

    Elimina el toEntity de la Uistate de authenticacion

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/authentication/UiState.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/authentication/UiState.kt
@@ -3,26 +3,12 @@ package ucne.edu.proyectofinalaplicada2.presentation.authentication
 import ucne.edu.proyectofinalaplicada2.data.remote.dto.ClienteDto
 
 data class UiState(
-    val clienteId: Int? = null,
-    val cedula: String? = null,
-    val nombre: String? = null,
-    val apellidos: String? = null,
-
-    val direccion: String? = null,
-    val celular: String? = null,
     val isLoading: Boolean = false,
     val error: String? = null,
     val success: String? = null,
     val email: String = "",
     val password: String = "",
-    val clientes: List<ClienteDto> = emptyList()
+    val errorEmail: String? = null,
+    val errorPassword: String? = null
 )
 
-fun UiState.toEntity() = ClienteDto(
-    clienteId = clienteId,
-    cedula = cedula,
-    nombre = nombre,
-    apellido = apellidos,
-    direccion = direccion,
-    celular = celular
-)
