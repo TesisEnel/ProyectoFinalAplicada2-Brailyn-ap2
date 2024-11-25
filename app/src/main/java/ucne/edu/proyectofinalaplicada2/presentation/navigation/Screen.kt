6237b5ea7a2325a12f6fa887233fa52a926f7df4
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-11-25 17:49:55 -0400

    Agrega fromRoute para convertir la ruta de string a tipo Screen

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/navigation/Screen.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/navigation/Screen.kt
@@ -1,8 +1,30 @@
 package ucne.edu.proyectofinalaplicada2.presentation.navigation
 
+import android.os.Bundle
 import kotlinx.serialization.Serializable
+import kotlin.reflect.KClass
+import kotlin.reflect.full.primaryConstructor
 
+@Serializable
 sealed class Screen {
+    companion object {
+        fun fromRoute(route: String, args: Bundle?): Screen? {
+            val subclass = Screen::class.sealedSubclasses.firstOrNull {
+                route.contains(it.qualifiedName.toString())
+            }
+            return subclass?.let { createInstance(it, args) }
+        }
+
+        private fun <T : Any> createInstance(kClass: KClass<T>, bundle: Bundle?): T? {
+            val constructor = kClass.primaryConstructor
+            return constructor?.let {
+                val args = it.parameters.associateWith { param ->
+                    bundle?.get(param.name)
+                }
+                it.callBy(args)
+            } ?: kClass.objectInstance
+        }
+    }
     @Serializable
     data object Home : Screen()
     @Serializable
