c790832d52dc74441fd38e8f718d68e37c2fa9d6
Eliezer Terrero Rojas <eliezerterrero12@gmail.com>
2024-11-26 23:57:13 -0400

    Crea ClienteDao

---
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/data/local/dao/ClienteDao.kt
@@ -0,0 +1,29 @@
+package ucne.edu.proyectofinalaplicada2.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import ucne.edu.proyectofinalaplicada2.data.local.entities.ClienteEntity
+
+@Dao
+interface ClienteDao {
+
+    @Upsert
+    suspend fun save(cliente: ClienteEntity)
+
+    @Query(
+        """select * from Clientes where clienteId=:id
+            Limit 1
+        """
+    )
+    suspend fun find(id: Int): ClienteEntity?
+
+    @Delete
+    suspend fun delete(cliente: ClienteEntity)
+
+    @Query("SELECT * FROM Clientes")
+    suspend fun getAll(): List<ClienteEntity>
+
+
+}
