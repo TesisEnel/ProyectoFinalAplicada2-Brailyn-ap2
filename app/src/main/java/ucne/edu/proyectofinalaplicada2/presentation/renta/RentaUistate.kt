b55bb85fd9e53ce3dd214f43d65b85eca102f916
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-11-27 10:47:22 -0400

    agrega vehiculoNombre, marca, vehiculo y cliente a RentaUistate

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/renta/RentaUistate.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/renta/RentaUistate.kt
@@ -1,6 +1,9 @@
 package ucne.edu.proyectofinalaplicada2.presentation.renta
 
+import ucne.edu.proyectofinalaplicada2.data.remote.dto.ClienteDto
+import ucne.edu.proyectofinalaplicada2.data.remote.dto.MarcaDto
 import ucne.edu.proyectofinalaplicada2.data.remote.dto.RentaDto
+import ucne.edu.proyectofinalaplicada2.data.remote.dto.VehiculoDto
 
 data class RentaUistate(
     val rentaId: Int? = null,
@@ -13,4 +16,8 @@ data class RentaUistate(
     val error: String? = null,
     val success: String? = null,
     val rentas: List<RentaDto> = emptyList(),
+    val vehiculoNombre: String? = null,
+    val marca: MarcaDto? = null,
+    val vehiculo: VehiculoDto? = null,
+    val cliente: ClienteDto? = null
 )
