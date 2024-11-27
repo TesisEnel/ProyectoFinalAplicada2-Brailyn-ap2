78d760371977e47411522037563dc59e87e9a20e
Eliezer Terrero Rojas <eliezerterrero12@gmail.com>
2024-11-26 23:57:30 -0400

    Crea RentaCarDb

---
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/data/local/database/RentCarDb.kt
@@ -0,0 +1,47 @@
+package ucne.edu.proyectofinalaplicada2.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import ucne.edu.proyectofinalaplicada2.data.local.dao.ClienteDao
+import ucne.edu.proyectofinalaplicada2.data.local.dao.MarcaDao
+import ucne.edu.proyectofinalaplicada2.data.local.dao.ModeloDao
+import ucne.edu.proyectofinalaplicada2.data.local.dao.ProveedorDao
+import ucne.edu.proyectofinalaplicada2.data.local.dao.RentaDao
+import ucne.edu.proyectofinalaplicada2.data.local.dao.TipoCombustibleDao
+import ucne.edu.proyectofinalaplicada2.data.local.dao.TipoVehiculoDao
+import ucne.edu.proyectofinalaplicada2.data.local.dao.VehiculoDao
+import ucne.edu.proyectofinalaplicada2.data.local.entities.ClienteEntity
+import ucne.edu.proyectofinalaplicada2.data.local.entities.MarcaEntity
+import ucne.edu.proyectofinalaplicada2.data.local.entities.ModeloEntity
+import ucne.edu.proyectofinalaplicada2.data.local.entities.ProveedorEntity
+import ucne.edu.proyectofinalaplicada2.data.local.entities.RentaEntity
+import ucne.edu.proyectofinalaplicada2.data.local.entities.TipoCombustibleEntity
+import ucne.edu.proyectofinalaplicada2.data.local.entities.TipoVehiculoEntity
+import ucne.edu.proyectofinalaplicada2.data.local.entities.VehiculoEntity
+
+@Database(
+    entities = [
+        ClienteEntity::class,
+        MarcaEntity::class,
+        ModeloEntity::class,
+        ProveedorEntity::class,
+        RentaEntity::class,
+        TipoCombustibleEntity::class,
+        TipoVehiculoEntity::class,
+        VehiculoEntity::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+
+abstract class RentCarDb : RoomDatabase() {
+    abstract fun clienteDao(): ClienteDao
+    abstract fun marcaDao(): MarcaDao
+    abstract fun modeloDao(): ModeloDao
+    abstract fun proveedorDao(): ProveedorDao
+    abstract fun rentaDao(): RentaDao
+    abstract fun tipoCombustibleDao(): TipoCombustibleDao
+    abstract fun tipoVehiculoDao(): TipoVehiculoDao
+    abstract fun vehiculoDao(): VehiculoDao
+
+}
