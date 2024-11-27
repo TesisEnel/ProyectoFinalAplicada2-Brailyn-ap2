852145c3f30767c4703907de4f1eea9e5f69198a
Eliezer Terrero Rojas <eliezerterrero12@gmail.com>
2024-11-26 23:53:52 -0400

    Crea VehiculoEntity

---
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/data/local/entities/VehiculoEntity.kt
@@ -0,0 +1,18 @@
+package ucne.edu.proyectofinalaplicada2.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Vehiculos")
+class VehiculoEntity(
+    @PrimaryKey
+    val vehiculoId: Int?,
+    val tipoCombustibleId: Int?,
+    val tipoVehiculoId: Int?,
+    val marcaId: Int?,
+    val modeloId: Int?,
+    val precio: Int?,
+    val descripcion: String?,
+    val anio: Int?,
+    val imagePath: List<String?>
+)
