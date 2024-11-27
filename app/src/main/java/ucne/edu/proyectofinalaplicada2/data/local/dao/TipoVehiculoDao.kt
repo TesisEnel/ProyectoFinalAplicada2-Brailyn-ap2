b8d4ee55ba6d3de3eabd5e986c485980d84887bd
Eliezer Terrero Rojas <eliezerterrero12@gmail.com>
2024-11-26 23:56:08 -0400

    Crea TipoVehiculoDao

---
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/data/local/dao/TipoVehiculoDao.kt
@@ -0,0 +1,27 @@
+package ucne.edu.proyectofinalaplicada2.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import ucne.edu.proyectofinalaplicada2.data.local.entities.TipoVehiculoEntity
+
+@Dao
+interface TipoVehiculoDao {
+    @Upsert
+    suspend fun save(tipoVehiculo: TipoVehiculoEntity)
+
+    @Query(
+        """select * from TiposVehiculos where tipoVehiculoId=:id
+            Limit 1
+        """
+    )
+    suspend fun find(id: Int): TipoVehiculoEntity?
+    @Delete
+    suspend fun delete(tipoVehiculo: TipoVehiculoEntity)
+
+    @Query("SELECT * FROM TiposVehiculos")
+    suspend fun getAll(): List<TipoVehiculoEntity>
+
+
+}
