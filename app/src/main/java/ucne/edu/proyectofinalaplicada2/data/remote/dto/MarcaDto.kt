71e54269cf0f2c13cf858fb2cbaa05d29b3c6d7e
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-11-29 00:18:46 -0400

    Agrega extension function al MarcaDto

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/data/remote/dto/MarcaDto.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/data/remote/dto/MarcaDto.kt
@@ -1,6 +1,12 @@
 package ucne.edu.proyectofinalaplicada2.data.remote.dto
 
+import ucne.edu.proyectofinalaplicada2.data.local.entities.MarcaEntity
+
 data class MarcaDto (
     val marcaId: Int,
     val nombreMarca: String,
 )
+fun MarcaDto.toEntity() = MarcaEntity(
+    marcaId = marcaId,
+    nombreMarca = nombreMarca,
+    )
