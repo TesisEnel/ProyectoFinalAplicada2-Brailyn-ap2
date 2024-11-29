845bc500b0ec1b317a7c4efdb8f74870d125b223
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-11-29 00:19:10 -0400

    Modifica GetMarcas para que sea offline first

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/repository/MarcaRepository.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/repository/MarcaRepository.kt
@@ -1,24 +1,32 @@
 package ucne.edu.proyectofinalaplicada2.repository
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
 import ucne.edu.proyectofinalaplicada2.data.local.dao.MarcaDao
-import ucne.edu.proyectofinalaplicada2.data.local.entities.VehiculoEntity
+import ucne.edu.proyectofinalaplicada2.data.local.entities.MarcaEntity
 import ucne.edu.proyectofinalaplicada2.data.remote.RentCarRemoteDataSource
-import ucne.edu.proyectofinalaplicada2.data.remote.dto.MarcaDto
 import ucne.edu.proyectofinalaplicada2.data.remote.dto.toEntity
 import ucne.edu.proyectofinalaplicada2.utils.Resource
 import javax.inject.Inject
 
 class MarcaRepository @Inject constructor(
     private val rentCarRemoteDataSource: RentCarRemoteDataSource,
+    private val marcaDao: MarcaDao
 ) {
-    fun getMarcas(): Flow<Resource<List<MarcaDto>>> = flow{
+    fun getMarcas(): Flow<Resource<List<MarcaEntity>>> = flow{
+        emit(Resource.Loading())
+        val localVehiculos = marcaDao.getAll().firstOrNull()
+        if(!localVehiculos.isNullOrEmpty()){
+            emit(Resource.Success(localVehiculos))
+        }
         try {
             emit(Resource.Loading())
             val marcas = rentCarRemoteDataSource.getMarcas()
-            emit(Resource.Success(marcas))
+            marcas.forEach{marca -> marcaDao.save(marca.toEntity())}
+            val updatedLocalMarcas = marcaDao.getAll().firstOrNull()
+            emit(Resource.Success(updatedLocalMarcas?: emptyList()))
         } catch (e: HttpException) {
             emit(Resource.Error("Error de internet ${e.message}"))
         } catch (e: Exception) {
@@ -26,14 +34,25 @@ class MarcaRepository @Inject constructor(
         }
     }
 
-    suspend fun getMarcaById(id: Int): Resource<MarcaDto>{
+    suspend fun getMarcaById(id: Int): Resource<MarcaEntity?>{
         return try {
-            val marca = rentCarRemoteDataSource.getMarcas().first{ it.marcaId == id}
+            val marca = marcaDao.getByMarcaIdId(id)
             Resource.Success(marca)
         } catch (e: HttpException) {
             Resource.Error("Error de internet ${e.message}")
         }
         catch (e: Exception) {
+            Resource.Error("Error desconocido ${e.message}")
+        }
+    }
+
+    suspend fun getNameMarca(marcaId: Int): Resource<MarcaEntity>{
+        return try {
+            val marcas = marcaDao.getByMarcaIdId(marcaId)
+            Resource.Success(marcas)
+        }catch ( e: HttpException){
+            Resource.Error("Error de internet ${e.message}")
+        }catch (e: Exception){
             Resource.Error("Error desconocido ${e.message}")
         }
     }
