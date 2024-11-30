594d887cd0cb0140ae456e7b54a8bf1e6a59c32e
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-11-30 18:59:16 -0400

    Agrega nueva key para guardar el email del usuario

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/utils/PreferenceKeys.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/utils/PreferenceKeys.kt
@@ -1,7 +1,9 @@
 package ucne.edu.proyectofinalaplicada2.utils
 
 import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
 
 object PreferenceKeys {
+    val currentUser = stringPreferencesKey("current_user")
     val role = booleanPreferencesKey("role")
 }
