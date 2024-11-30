72e7576c2cde354f73f288541c9d65589cfc5bdd
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-11-29 21:04:06 -0400

    Elimina metodo update renta del repository

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/repository/RentaRepository.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/repository/RentaRepository.kt
@@ -1,25 +1,35 @@
 package ucne.edu.proyectofinalaplicada2.repository
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
+import ucne.edu.proyectofinalaplicada2.data.local.dao.RentaDao
+import ucne.edu.proyectofinalaplicada2.data.local.entities.RentaEntity
 import ucne.edu.proyectofinalaplicada2.data.remote.RentCarRemoteDataSource
 import ucne.edu.proyectofinalaplicada2.data.remote.dto.RentaDto
+import ucne.edu.proyectofinalaplicada2.data.remote.dto.toEntity
 import ucne.edu.proyectofinalaplicada2.utils.Resource
 import javax.inject.Inject
 
 class RentaRepository @Inject constructor(
-    private val rentCarRemoteDataSource: RentCarRemoteDataSource
+    private val rentCarRemoteDataSource: RentCarRemoteDataSource,
+    private val rentaDao: RentaDao
 ) {
-     fun getRentas(): Flow<Resource<List<RentaDto>>> = flow {
+     fun getRentas(): Flow<Resource<List<RentaEntity>>> = flow {
         try {
             emit(Resource.Loading())
             val rentas = rentCarRemoteDataSource.getRentas()
-            emit(Resource.Success(rentas))
+            rentas.forEach{ vehiculo -> rentaDao.save(vehiculo.toEntity())}
+            val updatedLocalRentas = rentaDao.getAll().firstOrNull()
+            emit(Resource.Success(updatedLocalRentas?: emptyList()))
         } catch (e: HttpException) {
             emit(Resource.Error("Error de internet ${e.message}"))
         } catch (e: Exception) {
-            emit(Resource.Error("Error desconocido ${e.message}"))
+            val localRentas = rentaDao.getAll().firstOrNull()
+            if(!localRentas.isNullOrEmpty()){
+                emit(Resource.Success(localRentas))
+            }
         }
     }
     fun addRenta(rentaDto: RentaDto): Flow<Resource<RentaDto>> = flow {
@@ -33,16 +43,6 @@ class RentaRepository @Inject constructor(
             emit(Resource.Error("Error desconocido ${e.message}"))
         }
     }
-    fun updateRenta(id:Int, rentaDto: RentaDto): Flow<Resource<RentaDto>> = flow {
-        try {
-            emit(Resource.Loading())
-            val renta = rentCarRemoteDataSource.updateRenta(id,rentaDto)
-            emit(Resource.Success(renta))
-            } catch (e: HttpException) {
-            emit(Resource.Error("Error de internet ${e.message}"))
-        } catch (e: Exception) {
-            emit(Resource.Error("Error desconocido ${e.message}"))
-        }
-    }
+
 
 }
