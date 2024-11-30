a54c2fd42b046cd131fe9ba9323bcfcf6ea9e4dd
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-11-29 22:36:23 -0400

    Agrega toentity al Dto

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/data/remote/dto/TipoCombustibleDto.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/data/remote/dto/TipoCombustibleDto.kt
@@ -1,6 +1,13 @@
 package ucne.edu.proyectofinalaplicada2.data.remote.dto;
 
+import ucne.edu.proyectofinalaplicada2.data.local.entities.TipoCombustibleEntity
+
 data class TipoCombustibleDto (
     val tipoCombustibleId: Int,
     val nombreTipoCombustible: String
+)
+
+fun TipoCombustibleDto.toEntity()= TipoCombustibleEntity(
+    tipoCombustibleId = tipoCombustibleId,
+    nombreTipoCombustible = nombreTipoCombustible
 )
