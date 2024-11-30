9b5927bcb26c4d67c1455c947143b5d3df40134e
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-11-29 23:00:39 -0400

    Cambia TipoVehiculoEntity

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/tipovehiculo/TipoVehiculoUistate.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/tipovehiculo/TipoVehiculoUistate.kt
@@ -1,9 +1,9 @@
 package ucne.edu.proyectofinalaplicada2.presentation.tipovehiculo
 
-import ucne.edu.proyectofinalaplicada2.data.remote.dto.TipoVehiculoDto
+import ucne.edu.proyectofinalaplicada2.data.local.entities.TipoVehiculoEntity
 
 data class TipoVehiculoUistate(
-    val tipoVehiculos: List<TipoVehiculoDto> = emptyList(),
+    val tipoVehiculos: List<TipoVehiculoEntity> = emptyList(),
     val isLoading: Boolean = false,
     val error: String = "",
     val success: String = "",
