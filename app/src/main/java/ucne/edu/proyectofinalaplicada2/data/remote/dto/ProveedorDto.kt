64dac9c64fd397da1799476b430a31e1bcdff855
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-11-29 22:34:01 -0400

    Agrega toEntity al proveedorDto

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/data/remote/dto/ProveedorDto.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/data/remote/dto/ProveedorDto.kt
@@ -1,7 +1,15 @@
 package ucne.edu.proyectofinalaplicada2.data.remote.dto
 
+import ucne.edu.proyectofinalaplicada2.data.local.entities.ProveedorEntity
+
 data class ProveedorDto(
     val proveedorId: Int,
     val nombre: String,
     val celular: String,
+)
+
+fun ProveedorDto.toEntity()= ProveedorEntity(
+    proveedorId = proveedorId,
+    nombre = nombre,
+    celular = celular
 )
