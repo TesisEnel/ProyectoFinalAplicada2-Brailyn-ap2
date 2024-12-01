fabf2be1b07b9fe25c8d21cba982fd88ea15fde9
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-11-30 22:16:39 -0400

    Agrega clase GetModelosByMarcaId al event

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/modelo/ModeloEvent.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/modelo/ModeloEvent.kt
@@ -2,4 +2,5 @@ package ucne.edu.proyectofinalaplicada2.presentation.modelo
 
 interface ModeloEvent {
     data class GetModeloConVehiculos(val marcaId: Int) : ModeloEvent
+    data class GetVehiculosByMarcaId(val marcaId: Int) : ModeloEvent
 }
