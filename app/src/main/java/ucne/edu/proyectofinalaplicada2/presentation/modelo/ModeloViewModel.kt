6f30ab54386376b2a7f6d3666d0c967c0a611252
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-11-30 20:47:44 -0400

    Agrega VehiculoRepository y marcaRepository para llamar a sus respectivas funciones

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/modelo/ModeloViewModel.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/modelo/ModeloViewModel.kt
@@ -7,13 +7,17 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import ucne.edu.proyectofinalaplicada2.repository.MarcaRepository
 import ucne.edu.proyectofinalaplicada2.repository.ModeloRepository
+import ucne.edu.proyectofinalaplicada2.repository.VehiculoRepository
 import ucne.edu.proyectofinalaplicada2.utils.Resource
 import javax.inject.Inject
 
 @HiltViewModel
 class ModeloViewModel @Inject constructor(
-    private val modeloRepository: ModeloRepository
+    private val modeloRepository: ModeloRepository,
+    private val vehiculoRepository: VehiculoRepository,
+    private val marcaRepository: MarcaRepository
 ) : ViewModel() {
     private val _uistate = MutableStateFlow(ModeloUistate())
     val uistate = _uistate.asStateFlow()
@@ -45,13 +49,73 @@ class ModeloViewModel @Inject constructor(
                     is Resource.Success -> {
                         _uistate.update {
                             it.copy(
-                                modelos = result.data ?: emptyList()
-
+                                modelos = result.data ?: emptyList(),
+                                isLoading = false
                             )
                         }
                     }
                 }
             }
+        }
+    }
+    private fun getVehiculos(){
+        viewModelScope.launch {
+            vehiculoRepository.getVehiculos().collect { result ->
+                when (result) {
+                    is Resource.Error -> {
+                        _uistate.update {
+                            it.copy(
+                                error = result.message ?: "Error"
+                            )
+                        }
+                    }
+
+                    is Resource.Loading -> {
+                        _uistate.update {
+                            it.copy(
+                                isLoading = true
+                            )
+                        }
+                    }
+
+                    is Resource.Success -> {
+                        _uistate.update {
+                            it.copy(
+                                vehiculos = result.data ?: emptyList(),
+                                isLoading = false
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    private fun getModeloConVehiculos(marcaId: Int) {
+        val contador:Int  = 0
+        viewModelScope.launch {
+            val modeloConVehiculos = _uistate.value.vehiculos.map {
+                val vehiculo = vehiculoRepository.getVehiculoByMarcaId(marcaId).data
+                val marca = marcaRepository.getMarcaById(marcaId ).data
+                val modelo = modeloRepository.getModeloByMarcaId(marcaId).data
+                ModeloConVehiculo(
+                    nombreModelo = modelo?.modeloVehiculo?:"",
+                    vehiculo = vehiculo,
+                    marca = marca
+                )
+            }
+            _uistate.update {
+                it.copy(
+                    modeloConVehiculos = modeloConVehiculos
+                )
+            }
+        }
+    }
+
+    fun onEvent(event: ModeloEvent) {
+        when (event) {
+            is ModeloEvent.GetModeloConVehiculos -> { getModeloConVehiculos(event.marcaId) }
         }
     }
 }
