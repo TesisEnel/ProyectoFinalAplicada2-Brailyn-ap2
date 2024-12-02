5cc6608da1a0e741a1311f80d577d3efa94432f4
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-12-01 23:28:50 -0400

    Modifica un poco el diseno para mostrar las imagenes cuando se va a ctualizar el vehiculo

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/vehiculo/VehiculoRegistroScreen.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/vehiculo/VehiculoRegistroScreen.kt
@@ -10,13 +10,17 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedCard
@@ -25,22 +29,28 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil3.compose.AsyncImage
 import coil3.compose.rememberAsyncImagePainter
+import ucne.edu.proyectofinalaplicada2.R
 import ucne.edu.proyectofinalaplicada2.presentation.components.InputSelect
 import ucne.edu.proyectofinalaplicada2.presentation.permisos.PermisoGallery
+import ucne.edu.proyectofinalaplicada2.utils.Constant
 import java.io.File
 
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
@@ -66,13 +76,14 @@ fun VehiculoBodyRegistroScreen(
     onVehiculoEnvent: (VehiculoEvent) -> Unit,
     vehiculoId: Int,
 ) {
+    var showDialog by remember { mutableStateOf(false) }
     LaunchedEffect(key1 = vehiculoId) {
         if (vehiculoId > 0) {
 
             onVehiculoEnvent(VehiculoEvent.SelectedVehiculo(vehiculoId))
         }
     }
-    if (vehiculoUiState.isLoading == true && vehiculoId > 0) {
+    if (vehiculoUiState.isLoadingData == true && vehiculoId > 0) {
         Box(
             modifier = Modifier
                 .fillMaxSize(),
@@ -87,7 +98,7 @@ fun VehiculoBodyRegistroScreen(
             colors = CardDefaults.elevatedCardColors(MaterialTheme.colorScheme.surface),
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 45.dp, vertical = 30.dp),
+                .padding(horizontal = 25.dp, vertical = 30.dp),
         ) {
 
             Column(
@@ -173,21 +184,107 @@ fun VehiculoBodyRegistroScreen(
                         .fillMaxWidth()
                 )
                 PermisoGallery()
-                OutlinedButton(onClick = { onVehiculoEnvent(VehiculoEvent.Save) }) {
-                    Text(text = "Guardar")
+                OutlinedButton(onClick = {
+                    if(vehiculoId > 0){
+                        onVehiculoEnvent(VehiculoEvent.UpdateVehiculo)
+                    }else{
+
+                        onVehiculoEnvent(VehiculoEvent.Save)
+                    }
+                }) {
+                    Text(text = if(vehiculoId > 0) "Actualizar" else "Guardar")
                 }
                 if (vehiculoUiState.isLoadingData == true) {
                     LinearProgressIndicator()
                 }
 
+                Text(
+                    text = "Imágenes del Vehículo",
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(vertical = 8.dp)
+                )
+                LazyRow(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    items(vehiculoUiState.imagePath) { imageUrl ->
+                        AsyncImage(
+                            model = Constant.URL_BLOBSTORAGE + imageUrl,
+                            contentDescription = "Imagen del Vehículo",
+                            modifier = Modifier
+                                .size(150.dp)
+                                .padding(8.dp)
+                        )
+                    }
+                }
 
                 Text(text = vehiculoUiState.error, color = Color.Red)
+
+                if(vehiculoUiState.success.isNotEmpty()){
+                    if(!showDialog){
+                        CustomDialog(
+                            message = vehiculoUiState.success,
+                            isError = vehiculoUiState.success.isEmpty(),
+                            onDismiss = {showDialog = true}
+                        )
+                    }
+
+                }
             }
         }
 
     }
 
 }
+
+@Composable
+fun CustomDialog(
+    message: String,
+    isError: Boolean,
+    onDismiss: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(onClick = onDismiss) {
+                Text("Aceptar")
+            }
+        },
+        title = {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.check),
+                    contentDescription = null,
+                    modifier = Modifier.size(80.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+            }
+        },
+        text = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = message,
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = if (isError) Color.Red else Color.Black
+                )
+            }
+
+        },
+        containerColor = Color.White
+    )
+}
+
+
 
 
 @Composable
