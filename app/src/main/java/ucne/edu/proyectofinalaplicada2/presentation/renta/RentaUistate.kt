9d6e19a1a071d6c62bb98e2b3c68dddcbc0953f9
Eliezer Terrero Rojas <eliezerterrero12@gmail.com>
2024-12-02 01:52:01 -0400

    Agrega Propiedades necesarias para sustraer la informaciones de las distintas Entidades

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/renta/RentaUistate.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/renta/RentaUistate.kt
@@ -1,7 +1,10 @@
 package ucne.edu.proyectofinalaplicada2.presentation.renta
 
 import ucne.edu.proyectofinalaplicada2.data.local.entities.MarcaEntity
+import ucne.edu.proyectofinalaplicada2.data.local.entities.ModeloEntity
 import ucne.edu.proyectofinalaplicada2.data.local.entities.RentaEntity
+import ucne.edu.proyectofinalaplicada2.data.local.entities.TipoCombustibleEntity
+import ucne.edu.proyectofinalaplicada2.data.local.entities.TipoVehiculoEntity
 import ucne.edu.proyectofinalaplicada2.data.local.entities.VehiculoEntity
 import ucne.edu.proyectofinalaplicada2.data.remote.dto.ClienteDto
 import ucne.edu.proyectofinalaplicada2.data.remote.dto.ModeloDto
@@ -14,18 +17,30 @@ data class RentaUistate(
     val fechaRenta: String = "",
     val fechaEntrega: String? = null,
     val total: Double? = null,
+    val cantidadDias: Int? = null,
     val isLoading: Boolean = false,
     val error: String? = null,
+    val errorFechaRenta: String? = null,
+    val errorFechaEntrega: String? = null,
     val success: String? = null,
     val rentas: List<RentaEntity> = emptyList(),
     val vehiculoNombre: String? = null,
+    val vehiculoModelo: String? = null,
     val marca: MarcaEntity? = null,
+    val modelo : ModeloEntity? = null,
+    val tipoCombustibleEntity: TipoCombustibleEntity? = null,
+    val vehiculoConCombustible: String? = null,
+    val tipoVehiculoEntity: TipoVehiculoEntity? = null,
+    val vehiculoConTipo: String? = null,
     val vehiculo: VehiculoEntity? = null,
     val cliente: ClienteDto? = null,
     val renta: RentaDto? = null,
     val vehiculos: List<VehiculoEntity> = emptyList(),
     val modelos: List<ModeloDto> = emptyList(),
-    val rentaConVehiculo: List<RentaConVehiculo> = emptyList(),
+    val rentaConVehiculos: List<RentaConVehiculo> = emptyList(),
+    val rentaConVehiculo: RentaConVehiculo? = null,
+    val showModal: Boolean = false, // Control del modal
+    val fechaValida: Boolean = false
 
     )
 
@@ -34,6 +49,5 @@ data class RentaConVehiculo(
     val renta: RentaEntity? = null,
     val marca: MarcaEntity? = null,
     val anio: Int? = null,
+    val vehiculoEntity: VehiculoEntity? = null
 )
-
-
