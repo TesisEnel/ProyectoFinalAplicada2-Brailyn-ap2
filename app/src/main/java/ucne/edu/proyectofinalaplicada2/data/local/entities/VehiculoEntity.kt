0d7cc839ff4c4c1da8f3a56c1acab1b8551fa24b
Eliezer Terrero Rojas <eliezerterrero12@gmail.com>
2024-12-03 10:04:22 -0400

    Agrega un toVehiculoDto al VehiculoEntity

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/data/local/entities/VehiculoEntity.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/data/local/entities/VehiculoEntity.kt
@@ -2,9 +2,10 @@ package ucne.edu.proyectofinalaplicada2.data.local.entities
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import ucne.edu.proyectofinalaplicada2.data.remote.dto.VehiculoDto
 
 @Entity(tableName = "Vehiculos")
-class VehiculoEntity(
+data class VehiculoEntity(
     @PrimaryKey
     val vehiculoId: Int?,
     val tipoCombustibleId: Int?,
@@ -17,4 +18,18 @@ class VehiculoEntity(
     val imagePath: List<String?>?  ,
     val proveedorId: Int?,
     val estaRentado: Boolean?,
+)
+
+fun VehiculoEntity.toVehiculoDto() = VehiculoDto(
+    vehiculoId = vehiculoId,
+    tipoCombustibleId = tipoCombustibleId,
+    tipoVehiculoId = tipoVehiculoId,
+    marcaId = marcaId,
+    modeloId = modeloId,
+    precio = precio,
+    descripcion = descripcion,
+    anio = anio,
+    imagePath = imagePath,
+    proveedorId = proveedorId,
+    estaRentado = estaRentado,
 )
