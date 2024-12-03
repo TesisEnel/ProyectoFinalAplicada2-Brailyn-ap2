d6bd777f709ecc2778221ffe7be8066d1e4df90c
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-12-02 22:08:31 -0400

    Agrega toEntity a la BD

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/data/remote/dto/ClienteDto.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/data/remote/dto/ClienteDto.kt
@@ -1,5 +1,7 @@
 package ucne.edu.proyectofinalaplicada2.data.remote.dto
 
+import ucne.edu.proyectofinalaplicada2.data.local.entities.ClienteEntity
+
 data class ClienteDto(
     val clienteId: Int?,
     val cedula: String?,
@@ -9,4 +11,15 @@ data class ClienteDto(
     val celular: String?,
     val email: String?,
     val isAdmin: Boolean?,
+)
+
+fun ClienteDto.toEntity() = ClienteEntity(
+    clienteId = clienteId,
+    cedula = cedula,
+    nombre = nombre,
+    apellido = apellido,
+    direccion = direccion,
+    celular = celular,
+    email = email,
+    isAdmin = isAdmin,
 )
