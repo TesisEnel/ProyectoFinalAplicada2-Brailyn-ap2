345fd33202af8acba0babc2c327b2c193258b87b
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-12-03 23:36:40 -0400

    Elimina object Loading que no se utiliza

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/authentication/AuthViewModel.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/authentication/AuthViewModel.kt
@@ -579,7 +579,6 @@ class AuthViewModel @Inject constructor(
 sealed class AuthState {
     data object Authenticated : AuthState()
     data object Unauthenticated : AuthState()
-    data object Loading : AuthState()
     data class Error(val message: String) : AuthState()
 }
 
