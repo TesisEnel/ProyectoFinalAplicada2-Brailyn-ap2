b2a95a5fdb89e8399c80b63606d8ea6ad0e3dd9d
Eliezer Terrero Rojas <eliezerterrero12@gmail.com>
2024-12-04 15:36:15 -0400

    Agrega los metodos necesarios al Event

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/proveedor/ProveedorEvent.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/proveedor/ProveedorEvent.kt
@@ -2,4 +2,10 @@ package ucne.edu.proyectofinalaplicada2.presentation.proveedor
 
 sealed interface ProveedorEvent {
     data class OnChangeProveedorID(val proveedorId: Int) : ProveedorEvent
+    data class OnChangeNombre(val nombre: String) : ProveedorEvent
+    data class OnChangeTelefono(val telefono: String) : ProveedorEvent
+    data object OnSave : ProveedorEvent
+    data object OnClearSuccess : ProveedorEvent
+
+
 }
