dc5fc0bb44400a3d97550c327b779ebf1c423017
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-12-03 23:34:43 -0400

    Agrega metodo setFilter para filtrar por esatdo de vehiculo

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/modelo/ModeloEvent.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/presentation/modelo/ModeloEvent.kt
@@ -1,6 +1,8 @@
 package ucne.edu.proyectofinalaplicada2.presentation.modelo
 
 interface ModeloEvent {
-    data class GetModeloConVehiculos(val marcaId: Int) : ModeloEvent
-    data class GetVehiculosByMarcaId(val marcaId: Int) : ModeloEvent
+    data class GetModeloConVehiculos(val marcaId: Int,val isAdmin:Boolean) : ModeloEvent
+    data class GetVehiculosByMarcaId(val marcaId: Int, val isAdmin:Boolean) : ModeloEvent
+    data class SetFilter(val filter: ModeloFilter) : ModeloEvent
+
 }
