301d8cd4b9cea93820689493ee36b0f217d7b145
Eliezer Terrero Rojas <eliezerterrero12@gmail.com>
2024-12-04 15:37:47 -0400

    Elimina funcion que no se usa

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/repository/AuthRepository.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/repository/AuthRepository.kt
@@ -11,15 +11,12 @@ import javax.inject.Inject
 
 class AuthRepository @Inject constructor(
 ) {
-
     fun login(email: String, password: String): Flow<Resource<AuthState>> = flow {
         if (email.isBlank() || password.isBlank()) {
             emit(Resource.Error("Email o contraseña no pueden estar vacíos"))
             return@flow
         }
-
         emit(Resource.Loading())
-
         try {
             val authResult = FirebaseAuth.getInstance()
                 .signInWithEmailAndPassword(email, password)
@@ -35,24 +32,18 @@ class AuthRepository @Inject constructor(
             emit(Resource.Error( "Email o contraseña incorrectos"))
         }
     }
-
-
     fun signup(email: String, password: String): Flow<Resource<AuthState>> = flow {
         if (email.isBlank() || password.isBlank()) {
             emit(Resource.Error("No pueden estar vacíos"))
             return@flow
         }
-
         emit(Resource.Loading())
-
         try {
             val authResult = FirebaseAuth.getInstance()
                 .createUserWithEmailAndPassword(email, password)
                 .await()
-
             val user = authResult.user
             if (user != null) {
-
                 emit(Resource.Success(AuthState.Authenticated))
             } else {
                 emit(Resource.Error("User is null after successful registration"))
@@ -62,13 +53,4 @@ class AuthRepository @Inject constructor(
         }
     }
 
-    fun signout(): Flow<Resource<AuthState>> = flow {
-        emit(Resource.Loading())
-        try {
-            FirebaseAuth.getInstance().signOut()
-            emit(Resource.Success(AuthState.Unauthenticated))
-        } catch (e: Exception) {
-            emit(Resource.Error(e.message ?: "Signout failed"))
-        }
-    }
 }
